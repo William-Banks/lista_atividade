2bf5889add30589964062ad7a27697167b987b1a
KauanViniciusRibeiro <viniciuskauan805@gmail.com>
2024-08-27 10:49:08 -0300

    'Kauan'

---
+++ desafio1.py
@@ -0,0 +1,7 @@
+# declarando as variaveis
+numero1 = int(input('Digite um numero: '))
+numero2 = int(input('Digite outro numero'))
+# declarando a adição das variaveis
+soma = numero1 + numero2
+# declarando o resultado da adição das variaveis
+print('O resultado da soma entre os dois numeros digitados é: ', soma)
---
+++ desafio2.py
@@ -0,0 +1,7 @@
+# declarando as variaveis
+numero1 = int(input('Digite um numero: '))
+numero2 = int(input('Digite outro numero: '))
+# declarando a subtração das variaveis
+sub = numero1 - numero2
+# declarando o resultado da subtração das variaveis
+print('O resultado da subtração do primeiro pelo segundo numero é: ', sub)
